3c0e6d806ee9464111566ac5a92e38e40005d3a7
perjo927 <perjo927@student.liu.se>
2013-10-13 20:01:55 +0200

    Skapat kalendervy med datumväljare

--- src/com/example/lithetyst/MainActivity.java
+++ src/com/example/lithetyst/MainActivity.java
@@ -1,15 +1,53 @@
 package com.example.lithetyst;
 
 import android.os.Bundle;
-import android.app.Activity;
+
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+
+import java.util.Calendar;
+
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
+import android.widget.DatePicker;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    private CalendarView calendarView;
+    private int yr, mon, dy;
+    private Calendar selectedDate;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Calendar c = Calendar.getInstance();
+        yr = c.get(Calendar.YEAR);
+        mon = c.get(Calendar.MONTH);
+        dy = c.get(Calendar.DAY_OF_MONTH);
+        
+        calendarView = (CalendarView) findViewById(R.id.calendar_view);
+        Button datePickerButton = (Button) findViewById(R.id.date_picker_button);
+        
+        datePickerButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                  new DatePickerDialog(MainActivity.this, dateListener, yr, mon, dy).show();
+            }
+        });
+        
+        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                    Toast.makeText(getApplicationContext(), 
+                    		"Selected date is "+(month+1)+"-"+dayOfMonth+"-"+ year,
+                    		Toast.LENGTH_SHORT).show();
+                }
+            });
 	}
 
 	@Override
@@ -18,5 +56,21 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+    private DatePickerDialog.OnDateSetListener dateListener =
+    		new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                selectedDate=Calendar.getInstance();
+                
+                yr=year;
+                mon=monthOfYear;
+                dy=dayOfMonth;
+                
+                selectedDate.set(yr, mon, dy);
+               
+                calendarView.setDate(selectedDate.getTimeInMillis());
+            }
+        };
 
 }
