0f43b2e5dc4e7d86a313446884b0c8299934ee94
perjo927 <perjo927@student.liu.se>
2013-11-03 18:16:02 +0100

    Lagt till tabvy och actionbar

--- gen/com/example/lithetyst/R.java
+++ gen/com/example/lithetyst/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int actionbar_text=0x7f060000;
+        public static final int custom_theme_color=0x7f060001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -25,43 +26,74 @@ public final class R {
         public static final int text_size=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int action_about=0x7f020000;
+        public static final int action_help=0x7f020001;
+        public static final int action_settings=0x7f020002;
+        public static final int actionbar_background=0x7f020003;
+        public static final int actionbar_tab_indicator=0x7f020004;
+        public static final int actiontab_background=0x7f020005;
+        public static final int actiontab_background_selected=0x7f020006;
+        public static final int actiontab_background_selected_focused=0x7f020007;
+        public static final int actiontab_background_selected_focused_pressed=0x7f020008;
+        public static final int actiontab_background_selected_nonfocused_pressed=0x7f020009;
+        public static final int actiontab_background_unselected=0x7f02000a;
+        public static final int actiontab_background_unselected_focused=0x7f02000b;
+        public static final int actiontab_background_unselected_focused_pressed=0x7f02000c;
+        public static final int actiontab_background_unselected_nonfocused_pressed=0x7f02000d;
+        public static final int ic_action_search=0x7f02000e;
+        public static final int ic_launcher=0x7f02000f;
     }
     public static final class id {
-        public static final int action_search=0x7f090008;
-        public static final int action_settings=0x7f090007;
-        public static final int button1=0x7f090001;
-        public static final int button2=0x7f090002;
-        public static final int button3=0x7f090003;
-        public static final int calendar_view=0x7f090005;
+        public static final int action_about=0x7f09000d;
+        public static final int action_help=0x7f09000e;
+        public static final int action_settings=0x7f09000b;
+        public static final int action_settings_top=0x7f09000c;
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090005;
+        public static final int button3=0x7f090001;
+        public static final int button4=0x7f090003;
+        public static final int button5=0x7f090006;
+        public static final int calendar_view=0x7f090007;
         public static final int date_picker_button=0x7f090000;
-        public static final int edit_message=0x7f090004;
-        public static final int textView=0x7f090006;
+        public static final int edit_message=0x7f090002;
+        public static final int pager=0x7f090009;
+        public static final int section_label=0x7f09000a;
+        public static final int textView=0x7f090008;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_second=0x7f030002;
+        public static final int activity_tab=0x7f030003;
+        public static final int fragment_tab_dummy=0x7f030004;
     }
     public static final class menu {
         public static final int display_message=0x7f080000;
         public static final int main=0x7f080001;
         public static final int main_activity_actions=0x7f080002;
         public static final int second=0x7f080003;
+        public static final int tab=0x7f080004;
     }
     public static final class string {
-        public static final int action_search=0x7f050002;
-        public static final int action_settings=0x7f050001;
+        public static final int action_about=0x7f050001;
+        public static final int action_help=0x7f050002;
+        public static final int action_search=0x7f050004;
+        public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int button_display=0x7f050008;
-        public static final int button_off=0x7f050007;
-        public static final int button_on=0x7f050006;
-        public static final int edit_message=0x7f050009;
-        public static final int hello_world=0x7f050003;
-        public static final int open_date_picker=0x7f050004;
-        public static final int title_activity_display_message=0x7f05000a;
-        public static final int title_activity_second=0x7f050005;
+        public static final int button_disappointing=0x7f05000a;
+        public static final int button_display=0x7f050009;
+        public static final int button_off=0x7f050008;
+        public static final int button_on=0x7f050007;
+        public static final int button_tab=0x7f05000b;
+        public static final int edit_message=0x7f05000c;
+        public static final int hello_world=0x7f050005;
+        public static final int open_date_picker=0x7f050006;
+        public static final int title_activity_display_message=0x7f05000e;
+        public static final int title_activity_second=0x7f05000d;
+        public static final int title_activity_tab=0x7f05000f;
+        public static final int title_section1=0x7f050010;
+        public static final int title_section2=0x7f050011;
+        public static final int title_section3=0x7f050012;
     }
     public static final class style {
         /** 
@@ -98,7 +130,10 @@ public final class R {
         public static final int MyActionBar=0x7f070003;
         /**  ActionBar tabs text styles 
          */
-        public static final int MyActionBarTabText=0x7f070005;
+        public static final int MyActionBarTabText=0x7f070006;
+        /**  ActionBar tabs styles 
+         */
+        public static final int MyActionBarTabs=0x7f070005;
         /**  ActionBar title text 
          */
         public static final int MyActionBarTitleText=0x7f070004;
--- src/com/example/lithetyst/MainActivity.java
+++ src/com/example/lithetyst/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lithetyst;
+package com.example.lithetyst; // byt namn 
 
 import java.util.Calendar;
 
@@ -19,26 +19,32 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	// Kalenderdeklarationer
     private CalendarView calendarView;
     private int yr, mon, dy;
     private Calendar selectedDate;
-	
+	////
+    
+    // Hämtas av getIntent() 
     public final static String EXTRA_MESSAGE = "com.example.lithetyst.MESSAGE";
     
-    Button b1, b2;
+    // Bantonkod
+    //Button b1, b2;
 	VolumeController vc;// = new VolumeController();	
-	 
+	// 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-	
-		 b1 = (Button) findViewById(R.id.button1);
-		 b2 = (Button) findViewById(R.id.button2);
-		 
+		// På och av
+		 //b1 = (Button) findViewById(R.id.button1);
+		 //b2 = (Button) findViewById(R.id.button2);
+		 // knapplyssnare
 		 //createListeners();
 		 
+		//// KALENDERKOD ////
 		Calendar c = Calendar.getInstance();
         yr = c.get(Calendar.YEAR);
         mon = c.get(Calendar.MONTH);
@@ -47,6 +53,7 @@ public class MainActivity extends Activity {
         calendarView = (CalendarView) findViewById(R.id.calendar_view);
         Button datePickerButton = (Button) findViewById(R.id.date_picker_button);
         
+        // egen lyssnare för denna knapp
         datePickerButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                   new DatePickerDialog(MainActivity.this, dateListener, yr, mon, dy).show();
@@ -62,8 +69,9 @@ public class MainActivity extends Activity {
                 }
             });
 	}
+	//// SLUT KALENDERKOD ////
 	
-	/** Called when the user clicks the Send button */
+	/** button onclick = sendmessage! */
 	public void sendMessage(View view) {
 	    Intent intent = new Intent(this, DisplayMessageActivity.class);
 	    EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -72,7 +80,15 @@ public class MainActivity extends Activity {
 	    startActivity(intent);
 	}
 	
-	/** on button */
+	/** button onclick = startTabActivity! */
+	public void startTabActivity(View view) {
+	    Intent intent = new Intent(this, TabActivity.class);
+	    String message = "Nu vill vi öppna tab-vy";
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
+	
+	/** på/av */
 	public void unMute(View view) {
 	    // Do something in response to button
 		//vc.mute();		
@@ -84,7 +100,16 @@ public class MainActivity extends Activity {
 		System.out.println("mute");
 	}
 	
-	/*
+	
+    // 
+    public void startSecondActivity(View view) {
+    	   Intent intent = new Intent(this, SecondActivity.class);
+    	   intent.putExtra("BUTTON NUMBER", 1337);
+    	   startActivity(intent);
+    	}
+    
+	
+	/* Lyssnare
 	private void createListeners() {
         b1.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -101,7 +126,8 @@ public class MainActivity extends Activity {
         });
     }
     */
-	
+
+	// Main-meny! Top/bottom ?
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		
@@ -111,17 +137,24 @@ public class MainActivity extends Activity {
 	    return super.onCreateOptionsMenu(menu);
 		
 		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.main, menu);
-//		return true;
+	    // getMenuInflater().inflate(R.menu.main, menu);
+	    // return true;
 	}
 	
+	// Vad göra med menyval?
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    // Handle presses on the action bar items
 	    switch (item.getItemId()) {
-	        case R.id.action_search:
-	            //openSearch();
-	        	System.out.println("Search");
+        	case R.id.action_about:
+        		System.out.println("About");
+        		return true;
+        	case R.id.action_help:
+        		System.out.println("Help");
+        		return true;
+	        case R.id.action_settings_top:
+	            //openSettings();
+	        	System.out.println("Settings top");
 	            return true;
 	        case R.id.action_settings:
 	            //openSettings();
@@ -132,6 +165,7 @@ public class MainActivity extends Activity {
 	    }
 	}
 	
+	//// KALENDERKOD
     private DatePickerDialog.OnDateSetListener dateListener =
     		new DatePickerDialog.OnDateSetListener() {
 
@@ -147,11 +181,6 @@ public class MainActivity extends Activity {
                 calendarView.setDate(selectedDate.getTimeInMillis());
             }
         };
+        //// SLUT KALENDERKOD
 
-        private void startSecondActivity(int buttonNum) {
-        	   Intent intent = new Intent(this, SecondActivity.class);
-        	   intent.putExtra("BUTTON NUMBER", buttonNum);
-        	   startActivity(intent);
-        	}
-        
 }
