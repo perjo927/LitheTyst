d91063efec1641b283d9278631f2ffc1abcbec5c
perjo927 <perjo927@student.liu.se>
2013-11-05 15:49:35 +0100

    Gjort så att man välja en specifik dag och komma till en tabbad dagvy

--- src/com/example/lithetyst/MainActivity.java
+++ src/com/example/lithetyst/MainActivity.java
@@ -1,78 +1,30 @@
 package com.example.lithetyst; // byt namn 
 
-import java.util.Calendar;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.CalendarView.OnDateChangeListener;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
-	// Kalenderdeklarationer
-    private CalendarView calendarView;
-    private int yr, mon, dy;
-    private Calendar selectedDate;
-	////
-    
-    // Hämtas av getIntent() 
+
+	//
     public final static String EXTRA_MESSAGE = "com.example.lithetyst.MESSAGE";
     
-    // Bantonkod
-    //Button b1, b2;
-	VolumeController vc;// = new VolumeController();	
-	// 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// På och av
-		 //b1 = (Button) findViewById(R.id.button1);
-		 //b2 = (Button) findViewById(R.id.button2);
-		 // initiera knapplyssnare
-		 //createListeners();
-		
 		 
-		//// KALENDERKOD ////
-		Calendar c = Calendar.getInstance();
-        yr = c.get(Calendar.YEAR);
-        mon = c.get(Calendar.MONTH);
-        dy = c.get(Calendar.DAY_OF_MONTH);
-        
-        calendarView = (CalendarView) findViewById(R.id.calendar_view);
-        Button datePickerButton = (Button) findViewById(R.id.date_picker_button);
-        
-        // egen lyssnare för denna knapp
-        datePickerButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                  new DatePickerDialog(MainActivity.this, dateListener, yr, mon, dy).show();
-            }
-        });
-        
-        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
-                @Override
-                public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                    Toast.makeText(getApplicationContext(), 
-                    		"Selected date is "+(month+1)+"-"+dayOfMonth+"-"+ year,
-                    		Toast.LENGTH_SHORT).show();
-                }
-            });
 	}
-	//// SLUT KALENDERKOD ////
-	
-	
+
 	
 	/// onClick-händelser! ///
 	/** button onclick = sendmessage! */
@@ -90,12 +42,19 @@ public class MainActivity extends Activity {
 	}
 	
 	/** button onclick = startTabActivity! */
+	public void startCalendar(View view) {
+	    Intent intent = new Intent(this, CalendarActivity.class);
+	    startActivity(intent);
+	}
+	
+	// kalendervy
 	public void startTabActivity(View view) {
 	    Intent intent = new Intent(this, TabActivity.class);
 	    String message = "Nu vill vi öppna tab-vy";
 	    intent.putExtra(EXTRA_MESSAGE, message);
 	    startActivity(intent);
 	}
+	
 	
 	/** på/av */
 	public void unMute(View view) {
@@ -115,25 +74,8 @@ public class MainActivity extends Activity {
     	   startActivity(intent);
     	}  
 	/// slut onClick ///
-    
-	/* Lyssnare
-	private void createListeners() {
-        b1.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-            	//startSecondActivity(1);
-            }
-        });
-        b2.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-            	//startSecondActivity(1);
-            	//vc.mute();
-            }
-        });
-    }
-    */
 
+    
 	// Main-meny
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -171,26 +113,6 @@ public class MainActivity extends Activity {
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
-	// Slut meny
+	// Slut meny	
 	
-	
-	
-	
-	//// KALENDERKOD
-    private DatePickerDialog.OnDateSetListener dateListener =
-    		new DatePickerDialog.OnDateSetListener() {
-
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
-                selectedDate=Calendar.getInstance();
-                
-                yr=year;
-                mon=monthOfYear;
-                dy=dayOfMonth;
-                
-                selectedDate.set(yr, mon, dy);
-               
-                calendarView.setDate(selectedDate.getTimeInMillis());
-            }
-        };
-        //// SLUT KALENDERKOD
 }
