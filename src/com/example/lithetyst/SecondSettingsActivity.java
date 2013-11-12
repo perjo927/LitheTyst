9a4520125710f570b5bd9b3a185b0745148d2bce
perjo927 <perjo927@student.liu.se>
2013-11-12 13:21:08 +0100

    Buggfix i meny till settings B

--- src/com/example/lithetyst/SecondSettingsActivity.java
+++ src/com/example/lithetyst/SecondSettingsActivity.java
@@ -2,7 +2,10 @@ package com.example.lithetyst;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SecondSettingsActivity extends Activity {
 
@@ -14,9 +17,27 @@ public class SecondSettingsActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings_menu, menu);
-		return true;
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.settings_menu, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_calendar:
+	        	startTabActivity();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	public void startTabActivity() {
+		Intent intent = new Intent(this, TabActivity.class);
+		startActivity(intent);
 	}
 
 }
